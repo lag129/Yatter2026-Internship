38ba447d671a593c10d4ba718911773e091bbd05
lag129 <65485662+lag129@users.noreply.github.com>
2026-07-29 01:04:50 +0900

    パブリックタイムラインの実装

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter/ui/timeline/PublicTimelinePage.kt
@@ -0,0 +1,26 @@
+package com.dmm.bootcamp.yatter.ui.timeline
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun PublicTimelinePage(
+  publicTimelineViewModel: PublicTimelineViewModel = koinViewModel(),
+) {
+  val uiState by publicTimelineViewModel.uiState.collectAsStateWithLifecycle()
+
+  LifecycleEventEffect(event = Lifecycle.Event.ON_PAUSE) {
+    publicTimelineViewModel.onResume()
+  }
+
+  PublicTimelineTemplate(
+    yweetList = uiState.yweetList,
+    isLoading = uiState.isLoading,
+    isRefreshing = uiState.isRefreshing,
+    onRefresh = publicTimelineViewModel::onRefresh,
+  )
+}
