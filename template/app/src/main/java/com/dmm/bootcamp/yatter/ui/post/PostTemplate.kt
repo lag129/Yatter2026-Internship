8c824c0a98d7077e944a9e419f4f2de40f2d4169
lag129 <65485662+lag129@users.noreply.github.com>
2026-07-30 11:57:54 +0900

    ツイート機能2まで実装

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter/ui/post/PostTemplate.kt
@@ -0,0 +1,133 @@
+package com.dmm.bootcamp.yatter.ui.post
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.dmm.bootcamp.yatter.R
+import com.dmm.bootcamp.yatter.ui.post.bindingmodel.PostBindingModel
+import com.dmm.bootcamp.yatter.ui.theme.YatterTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PostTemplate(
+  postBindingModel: PostBindingModel,
+  isLoading: Boolean,
+  canPost: Boolean,
+  onYweetTextChanged: (String) -> Unit,
+  onClickPost: () -> Unit,
+  onClickNavIcon: () -> Unit,
+) {
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = {
+          Text(text = stringResource(R.string.post_title))
+        },
+        navigationIcon = {
+          IconButton(onClick = onClickNavIcon) {
+            Icon(
+              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+              contentDescription = stringResource(R.string.post_back_icon_button_description)
+            )
+          }
+        }
+      )
+    }
+  ) { paddingValues ->
+    Box(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(paddingValues),
+      contentAlignment = Alignment.Center,
+    ) {
+      Row(
+        modifier = Modifier.fillMaxSize()
+      ) {
+        AsyncImage(
+          model = postBindingModel.avatarUrl,
+          contentDescription = stringResource(R.string.post_avatar_content_description),
+          contentScale = ContentScale.Crop,
+          modifier = Modifier.size(64.dp)
+        )
+
+        Column(
+          horizontalAlignment = Alignment.End
+        ) {
+          TextField(
+            value = postBindingModel.yweetText,
+            onValueChange = onYweetTextChanged,
+            colors = androidx.compose.material.TextFieldDefaults.textFieldColors(
+              backgroundColor = Color.Transparent,
+              focusedIndicatorColor = Color.Transparent,
+              unfocusedIndicatorColor = Color.Transparent,
+              disabledIndicatorColor = Color.Transparent,
+            ),
+            placeholder = {
+              Text(text = stringResource(R.string.post_edit_field_placeholder))
+            },
+            modifier = Modifier
+              .fillMaxSize()
+              .weight(1f),
+          )
+
+          Button(
+            onClick = onClickPost,
+            enabled = canPost,
+            modifier = Modifier.padding(16.dp),
+          ) {
+            Text(text = stringResource(R.string.post_send_button_text))
+          }
+        }
+      }
+
+      if (isLoading) {
+        CircularProgressIndicator()
+      }
+    }
+  }
+}
+
+@Preview
+@Composable
+private fun PostTemplatePreview() {
+  YatterTheme {
+    Surface() {
+      PostTemplate(
+        postBindingModel = PostBindingModel(
+          avatarUrl = "https://avatars.githubusercontent.com/u/19385268?v=4",
+          yweetText = "",
+          attachmentImageUris = emptyList(),
+        ),
+        isLoading = false,
+        canPost = false,
+        onYweetTextChanged = {},
+        onClickPost = {},
+        onClickNavIcon = {},
+      )
+    }
+  }
+}
